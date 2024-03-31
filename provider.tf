6b7713c34004e7373d085e44f59dc433fc775750
Kelvison França <kelvison.vieira@live.com>
2024-03-31 17:12:00 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -2,9 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.43.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadecloudterraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
