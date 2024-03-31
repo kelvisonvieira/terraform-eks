551c2b2e1d748aa4516fa3e9bf77b1d4cb86985c
Kelvison França <kelvison.vieira@live.com>
2024-03-31 18:55:22 -0300

    feat:tfvars

---
+++ public.tf
@@ -0,0 +1,35 @@
+
+resource "aws_subnet" "eks_public_1a" {
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadecloud-subnet-1a",
+      "kubernetes.io/role/elb" = 1
+    }
+
+  )
+}
+
+resource "aws_subnet" "eks_public_1b" {
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadecloud-subnet-1b",
+      "kubernetes.io/role/elb" = 1
+    }
+
+  )
+
+
+
+}
